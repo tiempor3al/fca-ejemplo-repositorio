0dddb6418553207a661b444818fdc4750be0ffa7
Manuel <manuel@kingtide.mx>
2018-09-13 19:11:51 -0500

    Se cambio de nuevo el texto del tv

--- app/src/main/java/unam/fca/dmoviles/MainActivity.java
+++ app/src/main/java/unam/fca/dmoviles/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         //Para cambiar el texto de tv
-        tv.setText("Hola hoy es jueves");
+        tv.setText("Hola hoy es viernes");
 
         //Actividades
         //a) Modifica el texto "Hola mundo", pon tu nombre y ejecuta la aplicacion
